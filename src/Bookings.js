2657d803490ffb1dc46a446887d8b34f1b6c0a7a
Luke Sanger <Luke Sanger>
2023-08-04 14:08:51 +1000

    Adding styling to booking system

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,7 +4,7 @@ function Bookings(props) {
     return (
         <>
         <div id="bookingsTable">
-            <h1>The following bookings are available</h1>
+            <h1>Available bookings for {props.currentDate} </h1>
             {props.availableTimes.find(timeSlot => timeSlot.date == props.currentDate).times.map(time => <p>{time}</p>)}
         </div>
         </>
